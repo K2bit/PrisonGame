8357b549b0591013400436466658303620015bc8
Dean <dmartin34@academic.rrc.ca>
2026-01-20 03:49:17 -0600

    Added: health bar logic and asset to scene for player two

--- health_bar.gd
+++ health_bar.gd
@@ -1,6 +1,6 @@
 extends TextureRect
 
-func _on_player_1_health_changed(new_health: int):
+func _on_health_changed(new_health: int):
 		if new_health >= 100:
 			texture = load("res://assets/ui/health-bar/health-bar-100-player.png")
 		else:
